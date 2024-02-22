74e597fc1847584037011adeda40f6f08ad80daf
MaryanneGitau <160262584+MaryanneGitau@users.noreply.github.com>
2024-02-22 21:26:52 +0300

    Add files via upload

---
+++ User_input.py
@@ -0,0 +1,13 @@
+# user_input.py
+
+# Ask the user for their name
+name = input("Maryanne M. Gitau.")
+
+# Ask the user for their age
+age = input("25 years")
+
+# Ask the user for their location
+location = input("Kiambu ")
+
+# Print a personalized message
+print(f"Hello, {name}! You are {age} years old and live in {location}.")
